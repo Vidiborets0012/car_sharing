a85ca61beab0c2ae93c9bcf0eff5e0d887351339
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-12-01 17:54:00 +0200

    remove dots in the slider of the hero section

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ $(function () {
         arrows: false,
         dots: true,
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        dots: false
+      }
+    },
+  ]
     });
 
     $('.reviews-slider').slick({
